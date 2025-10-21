0bf057611d1c8d1730b1e281e52abc57ec897fb8
Jshebb <joaohsebbers@gmail.com>
2025-10-21 13:17:02 -0300

    chore: shadcn-config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // @ts-ignore: implicit any from JS module without declaration file
 import App from "./App";
 import "./index.css"; // se estiver usando Tailwind ou CSS global
@@ -7,6 +8,10 @@ import "./index.css"; // se estiver usando Tailwind ou CSS global
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,10 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // indica onde o Tailwind deve buscar classes
+    "index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': '#0F1420',
+        'gradient-start': '#00FFA3',
+        'gradient-end': '#7B68EE',
+      },
+      backgroundImage: {
+        'gradient-login': 'linear-gradient(to right, #00FFA3, #7B68EE)',
+      },
+    },
   },
   plugins: [],
-};
+}
