1470c29d7c33c5f5d526f34ae45e63c3b3ea93e4
Jshebb <joaohsebbers@gmail.com>
2025-10-21 11:22:03 -0300

    feat: Ts-config

--- client/src/components/ui/card.tsx
+++ client/src/components/ui/card.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+function cn(...classes: Array<string | false | null | undefined>) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)} {...props} />
