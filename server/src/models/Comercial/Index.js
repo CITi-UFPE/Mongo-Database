94672e1f54af57d6b17452e9860bd20a01bc7c0e
Pedro Henrique Ferreira da Silva <pedrohferreira005@gmail.com>
2025-11-19 10:57:00 -0300

    export models

---
+++ server/src/models/Comercial/Index.js
@@ -0,0 +1,27 @@
+const Contato = require('./Contato');
+const Empresa = require('./Empresa');
+const Fase_funil = require('./Fase_funil');
+const Historico_fase_lead = require('./Historico_fase_lead');
+const Interacao = require('./Interacao');
+const Lead = require('./Lead');
+const Membro = require('./Membro');
+const Meta = require('./Meta');
+const Motivo_perda = require('./Motivo_perda');
+const Nicho = require('./Nicho');
+const Origem_lead = require('./Origem_lead');
+const Vendedor = require('./Vendedor');
+
+module.exports = {
+  Contato,
+  Empresa,
+  Fase_funil,
+  Historico_fase_lead,
+  Interacao,
+  Lead,
+  Membro,
+  Meta,
+  Motivo_perda,
+  Nicho,
+  Origem_lead,
+  Vendedor
+};
